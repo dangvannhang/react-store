1057b8e2f77efd497b0966fb2e3c43971f0ecc82
dangvannhang <dangvannhang107@gmail.com>
2022-04-29 11:04:28 +0700

    feat[Nhang]: add Router and Switch for App

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import About from './AboutPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import PrivateRoute from './PrivateRoute'
+
+export {
+  Home,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  SingleProduct,
+  PrivateRoute,
+}
